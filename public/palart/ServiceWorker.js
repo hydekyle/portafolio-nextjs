b389effaea713bfb838e5b136272c71fb042b683
Ayoze Manuel <hydekyle@hotmail.com>
2023-02-17 05:12:50 +0000

    wp

--- public/palart/ServiceWorker.js
+++ public/palart/ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "Velocitech-PalArt Gallery-0.0.1";
+const contentToCache = [
+    "Build/palart.loader.js",
+    "Build/palart.framework.js.unityweb",
+    "Build/palart.data.unityweb",
+    "Build/palart.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
